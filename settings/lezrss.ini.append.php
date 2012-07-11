556db1d35b610da43288bcef24d3b794d446bf93
Guillaume Kulakowski <guillaume@llaumgui.com>
2012-07-11 11:54:58 +0200

    Doc

--- settings/lezrss.ini.append.php
+++ settings/lezrss.ini.append.php
@@ -1,6 +1,9 @@
 <?php /* #?ini charset="UTF-8"?
 
 [RSSSettings]
+
+# This parameter allow to prefix rss2/feed call.
+# You can use http://example.com/rss2/feed instead of  http://example.com/rss2/feed/news
 #DefaultRSS=news
 
 */ ?>
